cb82cbdae6eb8d76e35420c2a68b29abab60c13f
github_username <you@example.com>
2023-02-24 13:00:31 -0800

    wriiten correctly

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * mul - checks for checks for a digit (0 through 9).
- * @a: a - Variable
- * @b: b - variable
- * Return: Always 0.
+ * mul - multiplier
+ * @a: int to mult
+ * @b: int to mult
+ * Return: result of two
  */
 int mul(int a, int b)
 {
- int mul;
-
- mul = a * b;
+	return (a * b);
+}
