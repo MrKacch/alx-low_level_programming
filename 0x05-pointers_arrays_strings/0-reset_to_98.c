91c98e9b5106c3d270f88f46ddc92fa380130a34
github_username <you@example.com>
2023-02-28 08:20:06 -0800

    corrected

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n: pointer variable
  * Return: n
  */
 void reset_to_98(int *n)
